568a5b485da3c6a6dcad7aac4576060c23b8cf09
Adam K. Muro <bread@Adams-Air.home>
2018-03-12 21:52:12 -0400

    variables

---
+++ main.js
@@ -0,0 +1,8 @@
+var OTGrant18 = 1000;
+
+var NGOs = ["Help Unlimited", "New Start", "Better Tomorrow", "Home Safe", "New Home"];
+
+var MoExpenses = {
+  FamMems: [1, 2, 3, 4, 5],
+  Cost: [800, 950, 1000, 1100, 1200]
+};
